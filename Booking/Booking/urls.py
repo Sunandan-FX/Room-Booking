df1c42dacaae15ee3ae9f71af5b656aa76d5a216
Sunandan-FX <sunandandas131@gmail.com>
2025-08-19 21:03:54 +0600

    Update urls.py

--- Booking/Booking/urls.py
+++ Booking/Booking/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('',include('Order.urls')),
     path('admin/', admin.site.urls),
-    path('Order/',include('Order.urls')),
 ]
